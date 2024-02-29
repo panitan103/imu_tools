7110744d768e01e6dfade33cb5304a7994901917
panitan103 <panitan103_54@yahoo.com>
2024-02-29 19:28:54 +0700

    Update imu_filter.yaml and imu_filter.launch.py

--- imu_filter_madgwick/launch/imu_filter.launch.py
+++ imu_filter_madgwick/launch/imu_filter.launch.py
@@ -3,21 +3,34 @@ import launch
 import launch.actions
 import launch.substitutions
 import launch_ros.actions
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+
 from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
 
-    config_dir = os.path.join(get_package_share_directory('imu_filter_madgwick'), 'config')
+    config_dir = os.path.join(get_package_share_directory('imu_filter_madgwick'), 'config','imu_filter.yaml')
+    
+    use_mag_arg = DeclareLaunchArgument(
+        'use_mag',
+        default_value='False',
+        description='Use magnetometer data'
+    )
+
 
     return launch.LaunchDescription(
-        [
+        [   
+            use_mag_arg,
             launch_ros.actions.Node(
                 package='imu_filter_madgwick',
-                node_executable='imu_filter_madgwick_node',
-                node_name='imu_filter',
+                executable='imu_filter_madgwick_node',
+                name='imu_filter',
                 output='screen',
-                parameters=[os.path.join(config_dir, 'imu_filter.yaml')],
-            )
+                # parameters=[config_dir, {'use_mag': LaunchConfiguration('use_mag')}],
+                parameters=[config_dir, {'use_mag': LaunchConfiguration('use_mag')}],
+
+            ),
         ]
     )
